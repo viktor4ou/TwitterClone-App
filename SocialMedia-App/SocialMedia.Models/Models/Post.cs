0cd1af9c67ffdd80788e02bbc1ea32d1922751a1
Viktor Georgiev <71330381+viktor4ou@users.noreply.github.com>
2024-09-06 22:53:37 +0300

    -Add properties for Post model

--- SocialMedia-App/SocialMedia.Models/Models/Post.cs
+++ SocialMedia-App/SocialMedia.Models/Models/Post.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +10,12 @@ namespace SocialMedia.Models.Models
 {
     public class Post
     {
+        [Key]
         public int Id { get; set; }
-        public string Title { get; set; }
+        [Required]
         public string Content { get; set; }
-        public DateTime Created { get; set; }
+        public string? ImageURL { get; set; }
+        public int Likes { get; set; }
+        public DateTime DatePosted { get; set; }
     }
 }
