185fdf3b388fdc10336a37ed7c45ab6ee366aa08
Viktor Georgiev <71330381+viktor4ou@users.noreply.github.com>
2024-09-06 22:55:44 +0300

    -Create migrations for post table

---
+++ SocialMedia-App/SocialMedia-Data/Migrations/20240906191415_CreatePostsTable.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace SocialMedia.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatePostsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageURL = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Likes = table.Column<int>(type: "int", nullable: false),
+                    DatePosted = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "DatePosted", "ImageURL", "Likes" },
+                values: new object[,]
+                {
+                    { 1, "This is the content of the first post", new DateTime(2024, 9, 6, 22, 14, 13, 892, DateTimeKind.Local).AddTicks(6435), null, 0 },
+                    { 2, "This is the content of the second post", new DateTime(2024, 9, 6, 22, 14, 13, 892, DateTimeKind.Local).AddTicks(6502), null, 0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
