1c99368aaf3d5571f569314e9405ec5ddbf186e1
Viktor Georgiev <71330381+viktor4ou@users.noreply.github.com>
2024-09-06 22:54:20 +0300

    -Create ApplicationDbContext

--- SocialMedia-App/SocialMedia-App/Program.cs
+++ SocialMedia-App/SocialMedia-App/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SocialMedia.Data.Data;
+
 namespace SocialMedia_App
 {
     public class Program
@@ -8,9 +11,10 @@ namespace SocialMedia_App
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
-
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             var app = builder.Build();
-
+                
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
---
+++ SocialMedia-App/SocialMedia-Data/Data/ApplicationDbContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SocialMedia.Models.Models;
+
+namespace SocialMedia.Data.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+                
+        }
+        public DbSet<Post> Posts  { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Post>().HasData(
+                new Post
+                {
+                    Id = 1,
+                    Content = "This is the content of the first post",
+                    DatePosted = DateTime.Now
+                },
+                new Post
+                {
+                    Id = 2,
+                    Content = "This is the content of the second post",
+                    DatePosted = DateTime.Now
+                }
+            );
+        }
+        
+    }
+}
