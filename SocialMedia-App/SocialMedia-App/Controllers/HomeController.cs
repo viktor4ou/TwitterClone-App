09d86205bbc274b17e82f6d54d04b482dca13501
Viktor Georgiev <71330381+viktor4ou@users.noreply.github.com>
2024-09-07 20:26:37 +0300

    -Add viewModel for post -Add image upload fuctionality

--- SocialMedia-App/SocialMedia-App/Controllers/HomeController.cs
+++ SocialMedia-App/SocialMedia-App/Controllers/HomeController.cs
@@ -2,29 +2,60 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using SocialMedia.Data.Data;
 using SocialMedia.Models.Models;
+using Microsoft.AspNetCore.Hosting;
+using SocialMedia.Models.ViewModels;
 
 namespace SocialMedia_App.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ApplicationDbContext db;
-        public HomeController(ApplicationDbContext db)
+        private readonly IWebHostEnvironment webHostEnvironment;
+
+        public HomeController(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment)
         {
+
             this.db = db;
+            this.webHostEnvironment = webHostEnvironment;
         }
 
+        
         public IActionResult Index()
         {
-            return View();
+            var viewModel = new PostViewModel
+            {
+                Post = new Post(),
+                Posts = db.Posts.ToList()
+            };
+            return View(viewModel);
         }
-
         [HttpPost]
-        public IActionResult Index(Post post)// TODO: rename to CreatePost ,add image functionality 
+        public IActionResult Index(PostViewModel postVM, IFormFile? file)// TODO: rename to CreatePost ,add image functionality 
         {
-            post.DatePosted = DateTime.Now;
-            db.Posts.Add(post);
-            db.SaveChanges();
-            return View();
+            if (ModelState.IsValid)
+            {
+                if (file != null)
+                {
+                    string wwwRootPath = webHostEnvironment.WebRootPath;
+                    string filename = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(wwwRootPath, @"images\posts");
+
+                    using (FileStream s = new FileStream(Path.Combine(productPath, filename), FileMode.Create))
+                    {
+                        file.CopyTo(s);
+                    }
+
+                    postVM.Post.ImageURL = @"\images\posts\" + filename;
+                }
+
+                postVM.Post.DatePosted = DateTime.Now;
+                db.Posts.Add(postVM.Post);
+                db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            postVM.Posts = db.Posts.ToList();
+            return View(postVM);
         }
         public IActionResult Privacy()
         {
