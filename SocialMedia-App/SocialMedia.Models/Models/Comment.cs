de9e0414833b7034210d9e14698375187138b6c0
Viktor Georgiev <71330381+viktor4ou@users.noreply.github.com>
2024-09-08 21:37:38 +0300

    -Add Comment model

---
+++ SocialMedia-App/SocialMedia.Models/Models/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Models.Models
+{
+    public class Comment
+    {
+        [ForeignKey("PostId")]
+        public int PostId { get; set; }
+        public int CommentId { get; set; }
+        public string Content { get; set; }
+        public DateTime DatePosted { get; set; }    
+    }
+}
