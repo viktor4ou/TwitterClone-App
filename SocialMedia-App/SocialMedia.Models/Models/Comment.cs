e9c233f6f8272611b70cb6fe3bc4bc0988399f78
Viktor Georgiev <71330381+viktor4ou@users.noreply.github.com>
2024-09-10 00:15:59 +0300

    -Add Comment model validation

--- SocialMedia-App/SocialMedia.Models/Models/Comment.cs
+++ SocialMedia-App/SocialMedia.Models/Models/Comment.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace SocialMedia.Models.Models
 {
@@ -12,12 +13,14 @@ namespace SocialMedia.Models.Models
     {
         [Key]
         public int CommentId { get; set; }
+        [Required]
         public string Content { get; set; }
+        [ValidateNever]
         public DateTime DatePosted { get; set; }
-        
+        [ValidateNever]
         public int PostId { get; set; }
-
         [ForeignKey("PostId")]
+        [ValidateNever]
         public Post Post { get; set; }
     }
 }
