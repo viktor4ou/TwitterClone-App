308a8303f5bba228e4d508f57d70804ed47648c5
Viktor Georgiev <71330381+viktor4ou@users.noreply.github.com>
2024-09-17 00:49:58 +0300

    -Change the EditPost modal to not closable

--- SocialMedia-App/SocialMedia.Models/Models/Post.cs
+++ SocialMedia-App/SocialMedia.Models/Models/Post.cs
@@ -15,7 +15,7 @@ namespace SocialMedia.Models.Models
         [Key]
         public int Id { get; set; }
         [Required]
-        [Length(2,5,  ErrorMessage = "Content should be between 1 and 255 symbols")]
+        [Length(1,255,  ErrorMessage = "Content should be between 1 and 255 symbols")]
         public string Content { get; set; }
         public string? ImageURL { get; set; }
         [ValidateNever]
