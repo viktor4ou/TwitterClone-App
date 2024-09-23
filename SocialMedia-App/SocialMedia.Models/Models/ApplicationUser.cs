bd50630d1f1a2fdada8d298b75ea0703c51677ed
Viktor Georgiev <71330381+viktor4ou@users.noreply.github.com>
2024-09-23 22:08:43 +0300

    -Extend idenityUser model

---
+++ SocialMedia-App/SocialMedia.Models/Models/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+
+namespace SocialMedia.Models.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string?  ProfileImageURL { get; set; }   
+
+    }
+}
