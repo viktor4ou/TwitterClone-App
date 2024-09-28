736395163624da9221e275931cf52fbf2076d8fa
Viktor Georgiev <71330381+viktor4ou@users.noreply.github.com>
2024-09-29 01:55:58 +0300

    -Add UI for profile page

---
+++ SocialMedia-App/SocialMedia-Data/Migrations/20240928224139_ExtendApplicationUser.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialMedia.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExtendApplicationUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Followers",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Following",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "CommentId",
+                keyValue: 1,
+                column: "DatePosted",
+                value: new DateTime(2024, 9, 29, 1, 41, 38, 424, DateTimeKind.Local).AddTicks(1410));
+
+            migrationBuilder.UpdateData(
+                table: "Posts",
+                keyColumn: "PostId",
+                keyValue: 1,
+                column: "DatePosted",
+                value: new DateTime(2024, 9, 29, 1, 41, 38, 424, DateTimeKind.Local).AddTicks(1222));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Followers",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Following",
+                table: "AspNetUsers");
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "CommentId",
+                keyValue: 1,
+                column: "DatePosted",
+                value: new DateTime(2024, 9, 27, 23, 45, 4, 923, DateTimeKind.Local).AddTicks(1933));
+
+            migrationBuilder.UpdateData(
+                table: "Posts",
+                keyColumn: "PostId",
+                keyValue: 1,
+                column: "DatePosted",
+                value: new DateTime(2024, 9, 27, 23, 45, 4, 923, DateTimeKind.Local).AddTicks(1706));
+        }
+    }
+}
