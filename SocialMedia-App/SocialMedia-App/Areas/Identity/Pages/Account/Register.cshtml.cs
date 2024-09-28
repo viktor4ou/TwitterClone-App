01aec61c5c41ec988cae5069e824333e4ede21f9
Viktor Georgiev <71330381+viktor4ou@users.noreply.github.com>
2024-09-28 23:07:00 +0300

    -Change image path on registration

--- SocialMedia-App/SocialMedia-App/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SocialMedia-App/SocialMedia-App/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -124,7 +124,7 @@ namespace SocialMedia_App.Areas.Identity.Pages.Account
                     {
                         string wwwRootPath = _webHostEnvironment.WebRootPath;
                         filename = Guid.NewGuid().ToString() + Path.GetExtension(Input.Image.FileName);
-                        string productPath = Path.Combine(wwwRootPath, @"images\profile");
+                        string productPath = Path.Combine(wwwRootPath, @"\images\profile");
 
                         using (FileStream stream = new FileStream(Path.Combine(productPath, filename), FileMode.Create))
                         {
@@ -137,7 +137,7 @@ namespace SocialMedia_App.Areas.Identity.Pages.Account
                     }
 
                     // Set the ProfileImageURL property
-                    user.ProfileImageURL = Path.Combine(@"images\profile", filename);
+                    user.ProfileImageURL = Path.Combine(@"\images\profile", filename);
 
                     // Update the user in the database
                     await _userManager.UpdateAsync(user);
