b841cc914c4dba7543e69f4f8d5925d814e0f37a
Viktor Georgiev <71330381+viktor4ou@users.noreply.github.com>
2024-09-29 00:19:46 +0300

    -Create profileController and view -Set the end points into the login partial

---
+++ SocialMedia-App/SocialMedia-App/Areas/User/Controllers/ProfileController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialMedia_App.Areas.User.Controllers
+{
+    public class ProfileController : Controller
+    {
+        [Area("User")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
