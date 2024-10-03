0193a7bb8652e6882f6234ecea9da6f94f2f303c
Viktor Georgiev <71330381+viktor4ou@users.noreply.github.com>
2024-10-03 20:48:13 +0300

    -Create Like model

---
+++ SocialMedia-App/SocialMedia.Models/Models/Like.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace SocialMedia.Models.Models
+{
+    public class Like
+    {
+        [Key]
+        public string LikeId { get; set; }
+        [ValidateNever]
+        public int PostId { get; set; }
+        [ValidateNever]
+        public string LikeOwnerId { get; set; }
+    }
+}
