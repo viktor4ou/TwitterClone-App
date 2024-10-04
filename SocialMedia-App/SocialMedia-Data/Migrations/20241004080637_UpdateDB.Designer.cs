5ad29690523b2d7c13545e8713732d022e7daa13
Viktor Georgiev <71330381+viktor4ou@users.noreply.github.com>
2024-10-04 21:40:36 +0300

    Migrations

--- SocialMedia-App/SocialMedia-Data/Migrations/20241004080637_UpdateDB.Designer.cs
+++ SocialMedia-App/SocialMedia-Data/Migrations/20241004080637_UpdateDB.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SocialMedia.Data.Data;
 
@@ -11,9 +12,11 @@ using SocialMedia.Data.Data;
 namespace SocialMedia.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241004080637_UpdateDB")]
+    partial class UpdateDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SocialMedia-App/SocialMedia-Data/Migrations/20241004080637_UpdateDB.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialMedia.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "CommentId",
+                keyValue: 1,
+                column: "DatePosted",
+                value: new DateTime(2024, 10, 4, 11, 6, 36, 9, DateTimeKind.Local).AddTicks(7438));
+
+            migrationBuilder.UpdateData(
+                table: "Posts",
+                keyColumn: "PostId",
+                keyValue: 1,
+                column: "DatePosted",
+                value: new DateTime(2024, 10, 4, 11, 6, 36, 9, DateTimeKind.Local).AddTicks(7149));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "CommentId",
+                keyValue: 1,
+                column: "DatePosted",
+                value: new DateTime(2024, 10, 3, 21, 14, 15, 200, DateTimeKind.Local).AddTicks(3587));
+
+            migrationBuilder.UpdateData(
+                table: "Posts",
+                keyColumn: "PostId",
+                keyValue: 1,
+                column: "DatePosted",
+                value: new DateTime(2024, 10, 3, 21, 14, 15, 200, DateTimeKind.Local).AddTicks(3397));
+        }
+    }
+}
