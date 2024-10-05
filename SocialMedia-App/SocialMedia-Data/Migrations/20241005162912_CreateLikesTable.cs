a076d8bd877f32a7fc6d92b9072a0452b2410914
Viktor Georgiev <71330381+viktor4ou@users.noreply.github.com>
2024-10-05 19:49:35 +0300

    -Remove Seed data for like on creation

--- SocialMedia-App/SocialMedia-Data/Migrations/20241005162912_CreateLikesTable.cs
+++ SocialMedia-App/SocialMedia-Data/Migrations/20241005162912_CreateLikesTable.cs
@@ -6,42 +6,58 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace SocialMedia.Data.Migrations
 {
     /// <inheritdoc />
-    public partial class pisnami : Migration
+    public partial class CreateLikesTable : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.CreateTable(
+                name: "Likes",
+                columns: table => new
+                {
+                    LikeId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    PostId = table.Column<int>(type: "int", nullable: false),
+                    LikeOwnerId = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Likes", x => x.LikeId);
+                });
+
             migrationBuilder.UpdateData(
                 table: "Comments",
                 keyColumn: "CommentId",
                 keyValue: 1,
                 column: "DatePosted",
-                value: new DateTime(2024, 10, 3, 21, 14, 15, 200, DateTimeKind.Local).AddTicks(3587));
+                value: new DateTime(2024, 10, 5, 19, 29, 11, 621, DateTimeKind.Local).AddTicks(9201));
 
             migrationBuilder.UpdateData(
                 table: "Posts",
                 keyColumn: "PostId",
                 keyValue: 1,
                 column: "DatePosted",
-                value: new DateTime(2024, 10, 3, 21, 14, 15, 200, DateTimeKind.Local).AddTicks(3397));
+                value: new DateTime(2024, 10, 5, 19, 29, 11, 621, DateTimeKind.Local).AddTicks(9017));
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropTable(
+                name: "Likes");
+
             migrationBuilder.UpdateData(
                 table: "Comments",
                 keyColumn: "CommentId",
                 keyValue: 1,
                 column: "DatePosted",
-                value: new DateTime(2024, 10, 3, 20, 56, 34, 629, DateTimeKind.Local).AddTicks(8847));
+                value: new DateTime(2024, 9, 30, 21, 23, 40, 964, DateTimeKind.Local).AddTicks(2090));
 
             migrationBuilder.UpdateData(
                 table: "Posts",
                 keyColumn: "PostId",
                 keyValue: 1,
                 column: "DatePosted",
-                value: new DateTime(2024, 10, 3, 20, 56, 34, 629, DateTimeKind.Local).AddTicks(8658));
+                value: new DateTime(2024, 9, 30, 21, 23, 40, 964, DateTimeKind.Local).AddTicks(1853));
         }
     }
 }
