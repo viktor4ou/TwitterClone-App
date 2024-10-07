689c3f472f21eb1b014235e5273bea03b2c63835
Viktor Georgiev <71330381+viktor4ou@users.noreply.github.com>
2024-10-07 22:58:31 +0300

    -Add Followers List -Change FollowersCount property name

--- SocialMedia-App/SocialMedia.Models/ViewModels/ProfileViewModel.cs
+++ SocialMedia-App/SocialMedia.Models/ViewModels/ProfileViewModel.cs
@@ -14,9 +14,10 @@ namespace SocialMedia.Models.ViewModels
         public string FristName { get; set; }
         public string LastName { get; set; }
         public string ProfilePictureURL { get; set; }
-        public int Followers { get; set; }
+        public int FollowersCount { get; set; }
         public int Following { get; set; }
 
         public List<Post> Posts = new();
+        public List<Follower> Followers { get; set; }   
     }
 }
