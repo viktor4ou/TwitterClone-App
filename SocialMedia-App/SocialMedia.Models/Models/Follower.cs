9bf0173c367fa4a2ec0a7e486478b9e1b2ceecd8
Viktor Georgiev <71330381+viktor4ou@users.noreply.github.com>
2024-10-07 22:45:56 +0300

    -Add Follower model

---
+++ SocialMedia-App/SocialMedia.Models/Models/Follower.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Models.Models
+{
+    class Follower
+    {
+        [Key]
+        public string FollowerId { get; set; }
+
+        public string  UserId { get; set; }
+        public string FollowedUserId { get; set; }
+    }
+}
