55b6e3c80dcded17d92816e15ad3183b24f1388f
Viktor Georgiev <71330381+viktor4ou@users.noreply.github.com>
2024-10-09 23:06:20 +0300

    -Add ChatController

---
+++ SocialMedia-App/SocialMedia-App/Areas/User/Controllers/ChatController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Models.Models;
+
+namespace SocialMedia_App.Areas.User.Controllers
+{
+    [Area("User")]
+    public class ChatController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+        private readonly CustomUserManager customUserManager;
+        public ChatController(UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager,
+            CustomUserManager customUserManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.customUserManager = customUserManager;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
