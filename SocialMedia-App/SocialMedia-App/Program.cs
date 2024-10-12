e819e1836cc7c0e1b483ab62c9a88fbf4d45dc22
Viktor Georgiev <71330381+viktor4ou@users.noreply.github.com>
2024-10-12 20:25:43 +0300

    Add listening port

--- SocialMedia-App/SocialMedia-App/Program.cs
+++ SocialMedia-App/SocialMedia-App/Program.cs
@@ -11,7 +11,10 @@ namespace SocialMedia_App
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
+            builder.WebHost.ConfigureKestrel(options =>
+            {
+                options.ListenAnyIP(5000); // This binds to all network interfaces
+            });
             // Add services to the container.
             builder.Services.AddControllersWithViews();
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
