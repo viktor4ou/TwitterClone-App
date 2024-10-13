36d734962701ca079716792d9c4284ce7a571253
Viktor Georgiev <71330381+viktor4ou@users.noreply.github.com>
2024-10-13 20:27:18 +0300

    -Remove changes

--- SocialMedia-App/SocialMedia-App/wwwroot/js/chatCommunication.js
+++ SocialMedia-App/SocialMedia-App/wwwroot/js/chatCommunication.js
@@ -3,7 +3,6 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/chat").build();
 
 //Disable the send button until connection is established.
 document.getElementById("sendButton").disabled = true;
-var test = 123123213123;
 connection.on("ReceiveMessage", function (user, message) {
     var div = document.createElement("div");
     div.className = "container bg-white rounded-pill border text-dark p-2 my-2 rounded";
