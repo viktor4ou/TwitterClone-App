c164b50bfc34193bcb8f9bd06d2ac9b240240551
Viktor Georgiev <71330381+viktor4ou@users.noreply.github.com>
2024-10-13 18:09:19 +0300

    .

--- SocialMedia-App/SocialMedia-App/wwwroot/js/chatCommunication.js
+++ SocialMedia-App/SocialMedia-App/wwwroot/js/chatCommunication.js
@@ -1,6 +1,6 @@
 ﻿"use strict";
 
-var connection = new signalR.HubConnectionBuilder().withUrl("wss://echosocial.xyz/chat").build();
+var connection = new signalR.HubConnectionBuilder().withUrl("/chat").build();
 
 //Disable the send button until connection is established.
 document.getElementById("sendButton").disabled = true;
