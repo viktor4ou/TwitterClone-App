432262172c94308095b8a1e3124ad2b63a40cd12
Viktor Georgiev <71330381+viktor4ou@users.noreply.github.com>
2024-10-15 21:49:17 +0300

    -Change profile access by user id

--- SocialMedia-App/SocialMedia-App/Areas/User/Controllers/ProfileController.cs
+++ SocialMedia-App/SocialMedia-App/Areas/User/Controllers/ProfileController.cs
@@ -10,35 +10,45 @@ namespace SocialMedia_App.Areas.User.Controllers
     public class ProfileController : Controller
     {
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
         private readonly CustomUserManager _customUserManager;
+
         private readonly ApplicationDbContext _db;
-        public ProfileController(UserManager<IdentityUser> userManager, CustomUserManager customUserManager, ApplicationDbContext db)
+        public ProfileController(UserManager<IdentityUser> userManager, CustomUserManager customUserManager, ApplicationDbContext db,SignInManager<IdentityUser> signInManager)
         {
             _userManager = userManager;
             _customUserManager = customUserManager;
             _db = db;
+            _signInManager = signInManager;
         }
 
         [HttpGet]
-        public IActionResult Index()
+        public IActionResult Index(string userId)
         {
-            ProfileViewModel profileVM = GetProfileViewModel();
+            var currentLoggedUser = _signInManager.UserManager.GetUserAsync(User).Result as ApplicationUser;
+            if (currentLoggedUser is null)
+            {
+                return Unauthorized();
+            }
+
+            ProfileViewModel profileVM = GetProfileViewModel(userId);
             return View(profileVM);
         }
 
-        private ProfileViewModel GetProfileViewModel()
+        private ProfileViewModel GetProfileViewModel(string userId)
         {
             ProfileViewModel profileVM = new();
-            var user = _userManager.GetUserAsync(User).Result;
+            var user = _userManager.FindByIdAsync(userId).Result as ApplicationUser;
             if (user != null)
             {
                 profileVM.UserID = user.Id;
                 profileVM.Username = user.UserName;
-                profileVM.FristName = _customUserManager.GetFirstNameAsync(user as ApplicationUser).Result;
-                profileVM.LastName = _customUserManager.GetLastNameAsync(user as ApplicationUser).Result;
-                profileVM.ProfilePictureURL = _customUserManager.GetImageURLAsync(user as ApplicationUser).Result;
-                profileVM.FollowersCount = _customUserManager.GetFollowersCountAsync(user as ApplicationUser).Result;
-                profileVM.Following = _customUserManager.GetFollowingCountAsync(user as ApplicationUser).Result;
+                profileVM.FristName = _customUserManager.GetFirstNameAsync(user).Result;
+                profileVM.LastName = _customUserManager.GetLastNameAsync(user).Result;
+                profileVM.ProfilePictureURL = _customUserManager.GetImageURLAsync(user).Result;
+                profileVM.FollowersCount = _customUserManager.GetFollowersCountAsync(user).Result;
+                profileVM.Following = _customUserManager.GetFollowingCountAsync(user).Result;
+                profileVM.Followers = _db.Followers.ToList();
                 profileVM.Posts = _db.Posts.Where(i => i.PostOwnerId == user.Id).ToList();
             }
             return profileVM;
