928189adea1458f975b1cd972a13f90c534a247f
Viktor Georgiev <71330381+viktor4ou@users.noreply.github.com>
2024-10-16 23:09:40 +0300

    -Create ChatViewModel with list of users. Which is going to be used for displaying all the users that current user is following

---
+++ SocialMedia-App/SocialMedia.Models/ViewModels/ChatViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SocialMedia.Models.Models;
+
+namespace SocialMedia.Models.ViewModels
+{
+    public class ChatViewModel
+    {
+        public List<ApplicationUser> FollowedUsers = new();
+
+    }
+}
