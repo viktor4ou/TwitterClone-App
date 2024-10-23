888962d8f2038fde2c080e49bbeb43e58301d18e
Viktor Georgiev <71330381+viktor4ou@users.noreply.github.com>
2024-10-23 12:34:48 +0300

    -Add like repository

---
+++ SocialMedia-App/SocialMedia-Data/Repository/LikeRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using SocialMedia.Data.Data;
+using SocialMedia.Data.Interfaces;
+using SocialMedia.Models.Models;
+
+namespace SocialMedia.Data.Repository
+{
+    public class LikeRepository : Repository<Like>, ILIkeRepository
+    {
+        private readonly ApplicationDbContext db;
+        public LikeRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            db = dbContext;
+        }
+    }
+}
