d8c5fc076f752874048a30491644cef2fc56a385
Viktor Georgiev <71330381+viktor4ou@users.noreply.github.com>
2024-10-24 17:58:38 +0300

    -Create User repository

---
+++ SocialMedia-App/SocialMedia-Data/Repository/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SocialMedia.Data.Data;
+using SocialMedia.Data.Interfaces;
+using SocialMedia.Models.Models;
+
+namespace SocialMedia.Data.Repository
+{
+    public class UserRepository : Repository<ApplicationUser>, IUserRepository
+    {
+        private readonly ApplicationDbContext db;
+        public UserRepository(ApplicationDbContext dbContext):
+            base(dbContext)
+        {
+            db = dbContext;
+        }
+    }
+}
